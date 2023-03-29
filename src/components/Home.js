0bc0bb59917ca11c542eb80b5520dc1398e904b7
Moshik-laptop <moshiktm1994@gmail.com>
2023-03-29 18:03:31 +0300

    gpt

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,15 @@ import logo from '../moon.jpg'
 import axios from 'axios';
 import { useEffect} from 'react';
 import { Configuration, OpenAIApi } from "openai";
+import Button from 'react-bootstrap/Button';
+import ButtonSpinner from './Spinner';
 
 
 
 const HomePage = () => {
   const [searchText, setSearchText] = useState('');
   const [output, setOutput] = useState('');
+  const [spinner, setSpinner] = useState(false)
 
   useEffect(() => {
     if (output) {
@@ -62,21 +65,27 @@ const HomePage = () => {
       minHeight: "100vh"
     }}>
       <h1 className="heading">Infinity</h1>
-      <h1 className="html-spinner"></h1>
-      <h1 className="html-spinner1"></h1>
-      <h1 className="heading1">Space Market</h1>
+      <h1 class="underlined underline-overflow">Space Market</h1>
+      {/* <h1 className="html-spinner"></h1>
+      <h1 className="html-spinner1"></h1> */}
       <div>guy</div>
       <div>
         <div className='text-color'>Insert Customer Name</div>
         <form onSubmit={pass_c_name}>
           <input type="text" value={searchText} onChange={handleSearchChange} placeholder="Search..." />
-          <button type='submit'>Submit</button>
+          <Button onClick={() => {
+            setSpinner(true)
+            setTimeout(() => {setSpinner(false)}, 1000 * 3)
+          }}>
+          {spinner ? <div style={{ display: 'flex' }}> <ButtonSpinner/>  </div> : 'Submit'}
+          </Button>
+
         </form>
-        <div style={{color:"white"}}>mail: {output}</div>
+        <div style={{color:"white"}}>Mail: {output}</div>
       </div>
-      <div style={{fontSize:'200%',color:"white"}}>
+      {/* <div style={{fontSize:'200%',color:"white"}}>
         wait 2 minute for the result after clicking submit
-      </div>
+      </div> */}
     </div>
   );
 };
